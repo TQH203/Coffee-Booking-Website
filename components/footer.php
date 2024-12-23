84f6eabc662acab19396b6f62edf8565dfaeaea6
TQH203 <haotq.21it@vku.udn.vn>
2024-12-23 22:40:41 +0700

    Completed project

---
+++ components/footer.php
@@ -0,0 +1,38 @@
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <a href="tel:1234567890"><i class="fas fa-phone"></i>033-456-8848</a>
+         <a href="tel:1112223333"><i class="fas fa-phone"></i>039-607-9303</a>
+         <a href="mailto:shakhanas@gmail.com"><i class="fas fa-envelope"></i> ecokafe@gmail.com</a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i>51 Song Bo-Tu Ha,Huong Tra,Thua Thien Hue</a>
+      </div>
+
+      <div class="box">
+         <a href="#home">Home</a>
+         <a href="#about">About</a>
+         <a href="bookings.php">My bookings</a>
+         <a href="#reservation">Reservation</a>
+         <a href="#gallery">Gallery</a>
+         <a href="#contact">Contact</a>
+         <a href="#reviews">Reviews</a>
+      </div>
+
+      <div class="box">
+         <a href="https://www.facebook.com/tcht158/">Facebook<i class="fab fa-facebook"></i></a>
+         <a href="https://twitter.com/">Twitter <i class="fab fa-twitter"></i></a>
+         <a href="https://www.instagram.com/">Instagram <i class="fab fa-instagram"></i></a>
+         <a href="https://id.zalo.me/account?continue=https%3A%2F%2Fchat.zalo.me%2F">Zalo <i class="fab fa-viber"></i></a>
+         <a href="https://www.youtube.com/">Youtube <i class="fab fa-youtube"></i></a>
+      </div>
+
+   </div>
+
+   <div class="credit">&copy; copyright @ 2022 by mr. web designer | all rights reseved!</div>
+
+</section>
+
+<!-- footer section ends -->
